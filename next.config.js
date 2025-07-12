e0ff729822fd3e7f49ee85a8b1e58f79af1676ae
michaelabeltre <91342382+michaelabeltre@users.noreply.github.com>
2025-07-12 09:53:25 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // config options here
+  reactStrictMode: true,
 };
-
-// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-const { initOpenNextCloudflareForDev } = require("@opennextjs/cloudflare");
-initOpenNextCloudflareForDev();
 
 module.exports = nextConfig;

71c18ea5882ffcaad2c2414991c8c824ef40b061
michaelabeltre <91342382+michaelabeltre@users.noreply.github.com>
2025-07-12 09:37:22 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // config options here
 };
 
-export default nextConfig;
-
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+const { initOpenNextCloudflareForDev } = require("@opennextjs/cloudflare");
 initOpenNextCloudflareForDev();
+
+module.exports = nextConfig;
